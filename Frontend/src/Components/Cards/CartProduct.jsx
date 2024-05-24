e00e1780b7209ceac0aa150698e533e65458586e
ayush844 <ayush.xyz1625@gmail.com>
2024-05-24 22:40:02 +0530

    order summary in cart
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- Frontend/src/Components/Cards/CartProduct.jsx
+++ Frontend/src/Components/Cards/CartProduct.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const CartProduct = () => {
   return (
-    <div className=' flex items-center justify-between w-full border-b-[1px] border-black px-2 py-3'>
+    <div className=' flex flex-col md:flex-row gap-4 md:gap-0 items-center justify-between w-full border-b-[1px] border-black px-1 md:px-2 py-3'>
         <div id='productInfo' className=" flex items-center gap-3">
-            <div className=" w-32 h-40">
+            <div className=" w-24 h-28 md:w-32 md:h-40">
                 <img className=' w-full h-full object-cover' src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product image" />
             </div>
-            <h2 className=' line-clamp-2 text-xl font-normal w-96 cursor-pointer hover:text-blue-600 hover:underline'>Lux Cozi Men's Regular Fit Polo Neck Half Sleeve Solid Casual T-Shirt | Polo T-Shirt for Men</h2>
+            <h2 className=' line-clamp-3 md:line-clamp-2 text-lg md:text-xl font-normal w-48 sm:w-60 md:w-96 cursor-pointer hover:text-blue-600 hover:underline'>Lux Cozi Men's Regular Fit Polo Neck Half Sleeve Solid Casual T-Shirt | Polo T-Shirt for Men</h2>
         </div>
 
         <select class="py-2 px-3 pe-9 border-black bg-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none outline-none">
@@ -24,7 +24,7 @@ const CartProduct = () => {
         </select>
 
 
-        <div id="optionAndPrice" className=' flex flex-col items-center justify-evenly h-full'>
+        <div id="optionAndPrice" className=' flex flex-col items-center justify-evenly h-full gap-3'>
             <span className=' text-2xl'> $50</span>
             <span className=' text-blue-800 cursor-pointer'>REMOVE</span>
         </div>
--- Frontend/src/Pages/Cart.jsx
+++ Frontend/src/Pages/Cart.jsx
@@ -5,10 +5,10 @@ const Cart = () => {
   return (
     <main  className='min-h-screen overflow-x-hidden mb-6'>
     <div id="cartPage" className=' w-screen min-h-[100vh] lg:mt-12 lg:px-12 flex relative justify-center'>
-      <div id="cartProducts" className={`lg:flex flex-col p-0 w-[100vw] lg:w-[80vw] min-h-[100vh] m-0 lg:mt-0 bg-slate-100 mx-0 lg:mx-2 lg:rounded-lg lg:p-6`}>
+      <div id="cartProducts" className={`lg:flex flex-col p-0 w-[100vw] lg:w-[80vw] min-h-[100vh] m-0 lg:mt-0 bg-slate-100 mx-0 lg:mx-2 lg:rounded-lg lg:p-6 items-center`}>
 
         <div className="w-full h-fit py-4 pb-7 border-black border-b-2 flex justify-center">
-            <h1 className=' text-5xl font-normal uppercase '>Shopping Cart</h1>
+            <h1 className=' text-3xl md:text-5xl font-bold md:font-normal uppercase '>Shopping Cart</h1>
         </div>
 
         <div className="w-full h-fit p-12 flex flex-col">
@@ -18,6 +18,29 @@ const Cart = () => {
             <CartProduct />
             <CartProduct />
             <CartProduct />
+        </div>
+
+        <div id="checkoutCart" className=' w-[80%] h-fit min-h-56 flex flex-col items-center gap-10 bg-slate-300 rounded-xl p-8'>
+          <h2 className=' text-4xl font-normal uppercase '>Order Summary</h2>
+
+          <div className=" w-full">
+            <div className=" w-full h-fit border-gray-400 border-b flex items-center justify-between p-4">
+              <span className=' text-xl'>Total Price:</span>
+              <span className=' text-xl text-gray-700'>$500</span>
+            </div>
+            <div className=" w-full h-fit border-gray-400 border-b flex items-center justify-between p-4">
+              <span className=' text-xl'> Discount:</span>
+              <span className=' text-xl text-gray-700'>- $100</span>
+            </div>
+
+            <div className=" w-full h-fit border-gray-800 border-4 flex items-center justify-between p-4">
+              <span className=' text-2xl font-medium'> Order Total:</span>
+              <span className=' text-2xl font-medium text-gray-700'>$400</span>
+            </div>
+          </div>
+
+          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg w-full px-5 py-2.5 text-2xl text-center me-2 mb-2 uppercase font-bold">checkout</button>
+
         </div>
         
       </div>
