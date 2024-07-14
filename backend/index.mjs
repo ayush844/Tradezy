333db7d379c4600e71ef41d1c62e7293493c2e3c
ayush844 <ayush.xyz1625@gmail.com>
2024-07-14 23:55:26 +0530

    setting up dynamic routes
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- backend/index.mjs
+++ backend/index.mjs
@@ -6,6 +6,7 @@ import cors from 'cors';
 
 import { fileURLToPath } from 'url'
 import path from 'path';
+import { readdirSync } from 'fs';
 
 const app = express();
 
@@ -18,6 +19,16 @@ app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
+
+
+const routePaths = path.resolve(__dirname, "./routes");
+
+const routeFiles = readdirSync(routePaths);
+
+routeFiles.map(async(file)=>{
+    const routeModule = await import(`./routes/${file}`);
+    app.use('/', routeModule.default);
+})
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
