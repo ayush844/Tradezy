5e07a6f841f2eb2c2dfdfa5dcb514c4f2beb0446
ayush844 <ayush.xyz1625@gmail.com>
2024-07-21 06:50:26 +0530

    dynamic navbar for admin and normal user
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import { MdMenu } from "react-icons/md";
 import { CiLogin } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
-
+import { FaShoppingCart } from "react-icons/fa";
 import logo from '../../assets/logo.png'
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    console.log(user.user.email);
+
 
   return (
     <nav className='p-3 flex bg-white justify-between items-center border-b-2'>
@@ -19,11 +24,33 @@ const Navbar = () => {
         <div id="nav-menu" className='hidden md:flex gap-12'>
             <NavLink to="/categories" className={({isActive}) => `font-medium hover:text-primary text-lg ${isActive?"text-primary" : "" }`}>Categories</NavLink>
             <NavLink to="/products" className={({isActive}) => `font-medium hover:text-primary text-lg ${isActive?"text-primary" : "" }`}>Products</NavLink>
+            {
+                user?.user?.email &&
+                <>
+                    <NavLink to="/me" className={({isActive}) => `font-medium hover:text-primary text-lg ${isActive?"text-primary" : "" }`}>Me</NavLink>
+
+                </>
+            }
+
+            {
+                user?.user?.email === "admin@gmail.com" &&
+                <>
+                    <NavLink to="/admin/dashboard" className={({isActive}) => `font-medium hover:text-primary text-lg ${isActive?"text-primary" : "" }`}>Dashboard</NavLink>
+
+                </>
+            }
+
         </div>
-        <Link to='/login' className=' hidden md:flex items-center justify-center gap-3 rounded-lg border border-red-600 px-6 py-2 hover:border-2'>
-            <CiLogin className='text-primary text-2xl'/>
-            <span className='font-medium text-primary'>LogIn/SignUp</span>
-        </Link>
+
+        {!user ?
+            <Link to='/login' className=' hidden md:flex items-center justify-center gap-3 rounded-lg border border-red-600 px-6 py-2 hover:border-2'>
+                <CiLogin className='text-primary text-2xl'/>
+                <span className='font-medium text-primary'>LogIn/SignUp</span>
+            </Link>
+            :
+            <NavLink to="/cart" className={({isActive}) => `font-medium hover:text-primary text-lg hidden md:flex items-center gap-2 ${isActive?"text-primary" : "" }`}> <FaShoppingCart className=' text-2xl' /> Cart</NavLink>
+        }
+
         <button className='p-2 md:hidden'  onClick={()=>setIsOpen(prev => !prev)}>
             <MdMenu className='text-3xl'/>
         </button>
@@ -42,14 +69,34 @@ const Navbar = () => {
                 <Link to="/categories" className="font-medium m-3 p-3 block hover:bg-red-500 rounded-lg text-2xl text-white"  onClick={()=>setIsOpen(prev => !prev)}>
                     Categories
                 </Link>
+
                 <Link to="/products" className="font-medium m-3 p-3 block hover:bg-red-500 rounded-lg text-2xl text-white"  onClick={()=>setIsOpen(prev => !prev)}>
                     Products
                 </Link>
-                <div className="h-[1px] bg-gray-300"></div>
-                <Link to='/login' className='flex items-center justify-center gap-3 rounded-lg border-white px-3 py-1  border-2 m-4'  onClick={()=>setIsOpen(prev => !prev)}>
-                    <CiLogin className='text-white text-2xl'/>
-                    <span className='font-medium text-white text-2xl'>LogIn/SignUp</span>
+
+                <Link to="/me" className="font-medium m-3 p-3 block hover:bg-red-500 rounded-lg text-2xl text-white"  onClick={()=>setIsOpen(prev => !prev)}>
+                    Me
                 </Link>
+
+
+                <div className="h-[1px] bg-gray-300"></div>
+
+
+                {
+                !user ?
+                    <Link to='/login' className='flex items-center justify-center gap-3 rounded-lg border-white px-3 py-1  border-2 m-4'  onClick={()=>setIsOpen(prev => !prev)}>
+                        <CiLogin className='text-white text-2xl'/>
+                        <span className='font-medium text-white text-2xl'>LogIn/SignUp</span>
+                    </Link> 
+                    
+                    :
+
+                    <Link to="/cart" className="font-medium m-3 p-3 flex items-center gap-2 hover:bg-red-500 rounded-lg text-2xl text-white"  onClick={()=>setIsOpen(prev => !prev)}>
+                        <FaShoppingCart className=' text-2xl' /> 
+                        Cart
+                    </Link>
+
+                }
             </div>
         </div>
     </nav>
